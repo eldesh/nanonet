5befc646975d1a6249c76afef28dc8fddf2afdd6
eldesh <nephits@gmail.com>
2011-05-30 16:25:31 +0900

    publish functions for debugging

--- src/log.c
+++ src/log.c
@@ -31,7 +31,7 @@
 
 NANONET_LOG_TYPE nanonet_log_type = NANONET_LOG_DEBUGVIEW;
 
-static size_t const LOG_MSG_BUFF_SIZE = 1024;
+size_t const LOG_MSG_BUFF_SIZE = 1024;
 
 static
 int print_msg_impl( const char * file, const int line
